957fd9af59b9c2c29ae0d1f34de63106ce761086
RogerSGomes <rogersg.dev@gmail.com>
2023-07-04 14:59:09 -0300

    FEAT: Resetando DB

--- prisma/migrations/20230704131933_inti/migration.sql
+++
@@ -1,5 +0,0 @@
--- CreateTable
-CREATE TABLE "pops" (
-    "pop_id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "name" TEXT
-);
---
+++ prisma/migrations/20230704175826_init/migration.sql
@@ -0,0 +1,5 @@
+-- CreateTable
+CREATE TABLE "pops" (
+    "pop_id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT
+);
